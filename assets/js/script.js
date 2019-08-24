3c001ea332748ad9c4e553d03f943aad52f00bc9
Edward W. Lee <elee5696@gmail.com>
2019-08-23 19:07:11 -0700

    Finished Player Stats and Buying badges

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,126 @@ $(document).ready(initializeApp);
 var randomPaths = radomizeCardArr();
 
 var Game = {firstCard: 0, secondCard: null, matches: 0};
-var Player = {gold: 0, power: 0, streak: 0, accuracy: 0, totalMatches: 0,
-              totalGold: 0, totalAttempts: 0};
+var Player = {gold: 100, totalGold: 0, power: 0, streak: 0, accuracy: 0, totalMatches: 0,
+              totalAttempts: 0, badges: []};
+
+var badgeList = [
+  '../assets/img/miniheroes/abaddon.png',
+  '../assets/img/miniheroes/abyssal_underlord.png',
+  '../assets/img/miniheroes/alchemist.png',
+  '../assets/img/miniheroes/ancient_apparition.png',
+  '../assets/img/miniheroes/antimage.png',
+  '../assets/img/miniheroes/arc_warden.png',
+  '../assets/img/miniheroes/axe.png',
+  '../assets/img/miniheroes/bane.png',
+  '../assets/img/miniheroes/batrider.png',
+  '../assets/img/miniheroes/beastmaster.png',
+  '../assets/img/miniheroes/bloodseeker.png',
+  '../assets/img/miniheroes/bounty_hunter.png',
+  '../assets/img/miniheroes/brewmaster.png',
+  '../assets/img/miniheroes/bristleback.png',
+  '../assets/img/miniheroes/broodmother.png',
+  '../assets/img/miniheroes/centaur.png',
+  '../assets/img/miniheroes/chaos_knight.png',
+  '../assets/img/miniheroes/chen.png',
+  '../assets/img/miniheroes/clinkz.png',
+  '../assets/img/miniheroes/crystal_maiden.png',
+  '../assets/img/miniheroes/dark_seer.png',
+  '../assets/img/miniheroes/dark_willow.png',
+  '../assets/img/miniheroes/dazzle.png',
+  '../assets/img/miniheroes/death_prophet.png',
+  '../assets/img/miniheroes/disruptor.png',
+  '../assets/img/miniheroes/doom_bringer.png',
+  '../assets/img/miniheroes/dragon_knight.png',
+  '../assets/img/miniheroes/drow_ranger.png',
+  '../assets/img/miniheroes/earth_spirit.png',
+  '../assets/img/miniheroes/earthshaker.png',
+  '../assets/img/miniheroes/elder_titan.png',
+  '../assets/img/miniheroes/ember_spirit.png',
+  '../assets/img/miniheroes/enchantress.png',
+  '../assets/img/miniheroes/enigma.png',
+  '../assets/img/miniheroes/faceless_void.png',
+  '../assets/img/miniheroes/furion.png',
+  '../assets/img/miniheroes/grimstroke.png',
+  '../assets/img/miniheroes/gyrocopter.png',
+  '../assets/img/miniheroes/huskar.png',
+  '../assets/img/miniheroes/invoker.png',
+  '../assets/img/miniheroes/jakiro.png',
+  '../assets/img/miniheroes/juggernaut.png',
+  '../assets/img/miniheroes/keeper_of_the_light.png',
+  '../assets/img/miniheroes/kunkka.png',
+  '../assets/img/miniheroes/lanaya.png',
+  '../assets/img/miniheroes/legion_commander.png',
+  '../assets/img/miniheroes/leshrac.png',
+  '../assets/img/miniheroes/lich.png',
+  '../assets/img/miniheroes/life_stealer.png',
+  '../assets/img/miniheroes/lina.png',
+  '../assets/img/miniheroes/lion.png',
+  '../assets/img/miniheroes/lone_druid.png',
+  '../assets/img/miniheroes/luna.png',
+  '../assets/img/miniheroes/lycan.png',
+  '../assets/img/miniheroes/magnataur.png',
+  '../assets/img/miniheroes/medusa.png',
+  '../assets/img/miniheroes/meepo.png',
+  '../assets/img/miniheroes/mirana.png',
+  '../assets/img/miniheroes/monkey_king.png',
+  '../assets/img/miniheroes/morphling.png',
+  '../assets/img/miniheroes/naga_siren.png',
+  '../assets/img/miniheroes/necrolyte.png',
+  '../assets/img/miniheroes/nevermore.png',
+  '../assets/img/miniheroes/night_stalker.png',
+  '../assets/img/miniheroes/nyx_assassin.png',
+  '../assets/img/miniheroes/obsidian_destroyer.png',
+  '../assets/img/miniheroes/ogre_magi.png',
+  '../assets/img/miniheroes/omniknight.png',
+  '../assets/img/miniheroes/oracle.png',
+  '../assets/img/miniheroes/pangolier.png',
+  '../assets/img/miniheroes/phantom_assassin.png',
+  '../assets/img/miniheroes/phantom_lancer.png',
+  '../assets/img/miniheroes/phoenix.png',
+  '../assets/img/miniheroes/puck.png',
+  '../assets/img/miniheroes/pudge.png',
+  '../assets/img/miniheroes/pugna.png',
+  '../assets/img/miniheroes/queenofpain.png',
+  '../assets/img/miniheroes/rattletrap.png',
+  '../assets/img/miniheroes/razor.png',
+  '../assets/img/miniheroes/riki.png',
+  '../assets/img/miniheroes/rubick.png',
+  '../assets/img/miniheroes/sand_king.png',
+  '../assets/img/miniheroes/shadow_demon.png',
+  '../assets/img/miniheroes/shadow_shaman.png',
+  '../assets/img/miniheroes/shredder.png',
+  '../assets/img/miniheroes/silencer.png',
+  '../assets/img/miniheroes/skywrath_mage.png',
+  '../assets/img/miniheroes/slardar.png',
+  '../assets/img/miniheroes/sniper.png',
+  '../assets/img/miniheroes/slark.png',
+  '../assets/img/miniheroes/spectre.png',
+  '../assets/img/miniheroes/spirit_breaker.png',
+  '../assets/img/miniheroes/storm_spirit.png',
+  '../assets/img/miniheroes/sven.png',
+  '../assets/img/miniheroes/techies.png',
+  '../assets/img/miniheroes/terrorblade.png',
+  '../assets/img/miniheroes/tidehunter.png',
+  '../assets/img/miniheroes/tinker.png',
+  '../assets/img/miniheroes/tiny.png',
+  '../assets/img/miniheroes/treant.png',
+  '../assets/img/miniheroes/troll_warlord.png',
+  '../assets/img/miniheroes/tusk.png',
+  '../assets/img/miniheroes/undying.png',
+  '../assets/img/miniheroes/ursa.png',
+  '../assets/img/miniheroes/vengefulspirit.png',
+  '../assets/img/miniheroes/venomancer.png',
+  '../assets/img/miniheroes/viper.png',
+  '../assets/img/miniheroes/visage.png',
+  '../assets/img/miniheroes/warlock.png',
+  '../assets/img/miniheroes/weaver.png',
+  '../assets/img/miniheroes/windrunner.png',
+  '../assets/img/miniheroes/winter_wyvern.png',
+  '../assets/img/miniheroes/wisp.png',
+  '../assets/img/miniheroes/witch_doctor.png',
+  '../assets/img/miniheroes/zuus.png'];
+
 
 function initializeApp() {
   populateCards(radomizeCardArr());
@@ -15,7 +133,9 @@ function initializeApp() {
   { $('.win_modal_container').css('display', 'none'); })
   $('.player_stats').on('click', player_stats_open);
   $('.hero_stats').on('click', hero_stats_open);
+  $('.shop_menu_bar').on('click', shop_stats_open);
   $('.close').on('click', closeMenu);
+  $('.shop_start_button').on('click', addPlayerBadges);
 }
 
 function clickHandler(event) {
@@ -24,11 +144,21 @@ function clickHandler(event) {
 }
 
 function player_stats_open(event) {
+  var stat = $('.player_stats_box');
+  Player.accuracy = (( Player.totalMatches / Player.totalAttempts ) * 100).toFixed(2);
   $('.player_stats_content').parent('.stats_container').toggleClass('show');
+  stat.find('.gold_stat').text(Player.totalGold);
+  stat.find('.match_stat').text(Player.totalMatches);
+  stat.find('.attempts_stat').text(Player.totalAttempts);
+  stat.find('.acc_stat').text(Player.accuracy);
 }
 
 function hero_stats_open(event) {
   $('.hero_stats_content').parent('.stats_container').toggleClass('show');
+}
+
+function shop_stats_open(event) {
+  $('.shop_stats_content').parent('.stats_container').toggleClass('show');
 }
 
 function closeMenu() {
@@ -99,8 +229,11 @@ function winChecker() {
       $('.win_modal_container').css('display', 'flex');
       $('.card_inner').removeClass('card_flipped');
       $('.back').removeClass('is_matched');
+      $('.card_inner').toggleClass('reset_board');
       populateCards(radomizeCardArr());
-    }, 500);
+      setTimeout(function ()
+       { $('.card_inner').toggleClass('reset_board');}, 2000);
+    }, 200);
     Game.matches = 0;
   }
 }
@@ -196,8 +329,42 @@ function playErrorSound() {
 
 function playConfirm() {
   var confirmArr =
-    ['coins.wav',
-    'confirm.mp3',];
+    ['coins.wav'];
   var random = Math.floor((Math.random() * confirmArr.length));
   playAudio(confirmArr[random]);
+}
+
+function addPlayerBadges() {
+
+  if(Player.gold >= 50) {
+    if(badgeList.length) {
+      var random = Math.floor((Math.random() * badgeList.length));
+      Player.badges.push(badgeList[random]);
+      var wonBadge = badgeList[random];
+      badgeList.splice(random, 1);
+      playAudio('confirm.mp3');
+      $('.shop_screen_image').css('background-image', 'url(' + wonBadge + ')');
+      populateBadges();
+      Player.gold = Player.gold - 50;
+      $('.gold_counter').text(Player.gold);
+    } else {
+      console.log('Out of badges');
+    }
+  } else {
+    console.log("Not enough gold!");
+  }
+}
+
+function populateBadges() {
+
+  var badge = $('.player_stats_badge_container');
+
+  badge.empty();
+
+  if (Player.badges.length) {
+    for (keys of Player.badges) {
+      badge.append('<img src="' + keys + '">');
+    }
+  }
+
 }
