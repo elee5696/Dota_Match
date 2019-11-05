89c44015bf3ad4955deefee808b98d5857958f03
Edward W. Lee <elee5696@gmail.com>
2019-11-04 17:11:07 -0800

    removed unused assets

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,124 +6,6 @@ var Game = {firstCard: 0, secondCard: null, matches: 0};
 var Player = {gold: 0, totalGold: 0, power: 0, streak: 0, accuracy: 0, totalMatches: 0,
               totalAttempts: 0, gamesWon: 0, badges: []};
 
-var badgeList = [
-  'assets/img/miniheroes/abaddon.png',
-  'assets/img/miniheroes/abyssal_underlord.png',
-  'assets/img/miniheroes/alchemist.png',
-  'assets/img/miniheroes/ancient_apparition.png',
-  'assets/img/miniheroes/antimage.png',
-  'assets/img/miniheroes/arc_warden.png',
-  'assets/img/miniheroes/axe.png',
-  'assets/img/miniheroes/bane.png',
-  'assets/img/miniheroes/batrider.png',
-  'assets/img/miniheroes/beastmaster.png',
-  'assets/img/miniheroes/bloodseeker.png',
-  'assets/img/miniheroes/bounty_hunter.png',
-  'assets/img/miniheroes/brewmaster.png',
-  'assets/img/miniheroes/bristleback.png',
-  'assets/img/miniheroes/broodmother.png',
-  'assets/img/miniheroes/centaur.png',
-  'assets/img/miniheroes/chaos_knight.png',
-  'assets/img/miniheroes/chen.png',
-  'assets/img/miniheroes/clinkz.png',
-  'assets/img/miniheroes/crystal_maiden.png',
-  'assets/img/miniheroes/dark_seer.png',
-  'assets/img/miniheroes/dark_willow.png',
-  'assets/img/miniheroes/dazzle.png',
-  'assets/img/miniheroes/death_prophet.png',
-  'assets/img/miniheroes/disruptor.png',
-  'assets/img/miniheroes/doom_bringer.png',
-  'assets/img/miniheroes/dragon_knight.png',
-  'assets/img/miniheroes/drow_ranger.png',
-  'assets/img/miniheroes/earth_spirit.png',
-  'assets/img/miniheroes/earthshaker.png',
-  'assets/img/miniheroes/elder_titan.png',
-  'assets/img/miniheroes/ember_spirit.png',
-  'assets/img/miniheroes/enchantress.png',
-  'assets/img/miniheroes/enigma.png',
-  'assets/img/miniheroes/faceless_void.png',
-  'assets/img/miniheroes/furion.png',
-  'assets/img/miniheroes/grimstroke.png',
-  'assets/img/miniheroes/gyrocopter.png',
-  'assets/img/miniheroes/huskar.png',
-  'assets/img/miniheroes/invoker.png',
-  'assets/img/miniheroes/jakiro.png',
-  'assets/img/miniheroes/juggernaut.png',
-  'assets/img/miniheroes/keeper_of_the_light.png',
-  'assets/img/miniheroes/kunkka.png',
-  'assets/img/miniheroes/lanaya.png',
-  'assets/img/miniheroes/legion_commander.png',
-  'assets/img/miniheroes/leshrac.png',
-  'assets/img/miniheroes/lich.png',
-  'assets/img/miniheroes/life_stealer.png',
-  'assets/img/miniheroes/lina.png',
-  'assets/img/miniheroes/lion.png',
-  'assets/img/miniheroes/lone_druid.png',
-  'assets/img/miniheroes/luna.png',
-  'assets/img/miniheroes/lycan.png',
-  'assets/img/miniheroes/magnataur.png',
-  'assets/img/miniheroes/medusa.png',
-  'assets/img/miniheroes/meepo.png',
-  'assets/img/miniheroes/mirana.png',
-  'assets/img/miniheroes/monkey_king.png',
-  'assets/img/miniheroes/morphling.png',
-  'assets/img/miniheroes/naga_siren.png',
-  'assets/img/miniheroes/necrolyte.png',
-  'assets/img/miniheroes/nevermore.png',
-  'assets/img/miniheroes/night_stalker.png',
-  'assets/img/miniheroes/nyx_assassin.png',
-  'assets/img/miniheroes/obsidian_destroyer.png',
-  'assets/img/miniheroes/ogre_magi.png',
-  'assets/img/miniheroes/omniknight.png',
-  'assets/img/miniheroes/oracle.png',
-  'assets/img/miniheroes/pangolier.png',
-  'assets/img/miniheroes/phantom_assassin.png',
-  'assets/img/miniheroes/phantom_lancer.png',
-  'assets/img/miniheroes/phoenix.png',
-  'assets/img/miniheroes/puck.png',
-  'assets/img/miniheroes/pudge.png',
-  'assets/img/miniheroes/pugna.png',
-  'assets/img/miniheroes/queenofpain.png',
-  'assets/img/miniheroes/rattletrap.png',
-  'assets/img/miniheroes/razor.png',
-  'assets/img/miniheroes/riki.png',
-  'assets/img/miniheroes/rubick.png',
-  'assets/img/miniheroes/sand_king.png',
-  'assets/img/miniheroes/shadow_demon.png',
-  'assets/img/miniheroes/shadow_shaman.png',
-  'assets/img/miniheroes/shredder.png',
-  'assets/img/miniheroes/silencer.png',
-  'assets/img/miniheroes/skywrath_mage.png',
-  'assets/img/miniheroes/slardar.png',
-  'assets/img/miniheroes/sniper.png',
-  'assets/img/miniheroes/slark.png',
-  'assets/img/miniheroes/spectre.png',
-  'assets/img/miniheroes/spirit_breaker.png',
-  'assets/img/miniheroes/storm_spirit.png',
-  'assets/img/miniheroes/sven.png',
-  'assets/img/miniheroes/techies.png',
-  'assets/img/miniheroes/terrorblade.png',
-  'assets/img/miniheroes/tidehunter.png',
-  'assets/img/miniheroes/tinker.png',
-  'assets/img/miniheroes/tiny.png',
-  'assets/img/miniheroes/treant.png',
-  'assets/img/miniheroes/troll_warlord.png',
-  'assets/img/miniheroes/tusk.png',
-  'assets/img/miniheroes/undying.png',
-  'assets/img/miniheroes/ursa.png',
-  'assets/img/miniheroes/vengefulspirit.png',
-  'assets/img/miniheroes/venomancer.png',
-  'assets/img/miniheroes/viper.png',
-  'assets/img/miniheroes/visage.png',
-  'assets/img/miniheroes/warlock.png',
-  'assets/img/miniheroes/weaver.png',
-  'assets/img/miniheroes/windrunner.png',
-  'assets/img/miniheroes/winter_wyvern.png',
-  'assets/img/miniheroes/wisp.png',
-  'assets/img/miniheroes/witch_doctor.png',
-  'assets/img/miniheroes/zuus.png'];
-
-
 function initializeApp() {
   populateCards(radomizeCardArr());
   $('.card').on('click', clickHandler);
@@ -131,9 +13,9 @@ function initializeApp() {
   $('.arrow_container').on('click', menubarHandler);
   $('.modal_button').on('click', function ()
   { $('.win_modal_container').css('display', 'none'); })
-  $('.player_stats').on('click', player_stats_open);
-  $('.hero_stats').on('click', hero_stats_open);
-  $('.shop_menu_bar').on('click', shop_stats_open);
+  $('.player_stats').on('click', playerStatsOpen);
+  $('.hero_stats').on('click', heroStatsOpen);
+  $('.shop_menu_bar').on('click', shopStatsOpen);
   $('.close').on('click', closeMenu);
   $('.shop_start_button').on('click', addPlayerBadges);
 }
@@ -143,7 +25,7 @@ function clickHandler(event) {
   winChecker();
 }
 
-function player_stats_open(event) {
+function playerStatsOpen(event) {
   var stat = $('.player_stats_box');
   Player.accuracy = (( Player.totalMatches / Player.totalAttempts ) * 100).toFixed(2);
   $('.player_stats_content').parent('.stats_container').toggleClass('show');
@@ -158,11 +40,11 @@ function player_stats_open(event) {
   }
 }
 
-function hero_stats_open(event) {
+function heroStatsOpen(event) {
   $('.hero_stats_content').parent('.stats_container').toggleClass('show');
 }
 
-function shop_stats_open(event) {
+function shopStatsOpen(event) {
   $('.shop_stats_content').parent('.stats_container').toggleClass('show');
 }
 
@@ -253,44 +135,6 @@ function populateCards(arr) {
 }
 
 function radomizeCardArr() {
-  var imgPaths = [
-    'assets/img/items/abyssal_blade.png',
-    'assets/img/items/aeon_disk.png',
-    'assets/img/items/vladmir.png',
-    'assets/img/items/silver_edge.png',
-    'assets/img/items/skadi.png',
-    'assets/img/items/sheepstick.png',
-    'assets/img/items/radiance.png',
-    'assets/img/items/dagon_5.png',
-    'assets/img/items/armlet.png',
-    'assets/img/items/assault.png',
-    'assets/img/items/bfury.png',
-    'assets/img/items/black_king_bar.png',
-    'assets/img/items/butterfly.png',
-    'assets/img/items/crimson_guard.png',
-    'assets/img/items/desolator.png',
-    'assets/img/items/diffusal_blade_2.png',
-    'assets/img/items/ethereal_blade.png',
-    'assets/img/items/force_staff.png',
-    'assets/img/items/glimmer_cape.png',
-    'assets/img/items/guardian_greaves.png',
-    'assets/img/items/hand_of_midas.png',
-    'assets/img/items/heart.png',
-    'assets/img/items/heavens_halberd.png',
-    'assets/img/items/helm_of_the_dominator.png',
-    'assets/img/items/hurricane_pike.png',
-    'assets/img/items/lotus_orb.png',
-    'assets/img/items/meteor_hammer.png',
-    'assets/img/items/moon_shard.png',
-    'assets/img/items/necronomicon_3.png',
-    'assets/img/items/nullifier.png',
-    'assets/img/items/rapier.png',
-    'assets/img/items/refresher.png',
-    'assets/img/items/sange_and_yasha.png',
-    'assets/img/items/satanic.png',
-    'assets/img/items/shivas_guard.png',
-    'assets/img/items/sphere.png'];
-
   var randomArr = shuffleDeck(imgPaths);
   randomArr = shuffleDeck(randomArr.slice(0,12));
 
@@ -368,7 +212,7 @@ function populateBadges() {
   badge.empty();
 
   if (Player.badges.length) {
-    for (keys of Player.badges) {
+    for (var keys of Player.badges) {
       badge.append('<img src="' + keys + '">');
     }
   }
